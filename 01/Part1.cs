99d7d1e19163156506c3e1a8b817acc0f1c7d5f2
Lewis Wilson <57541885+lwlsns@users.noreply.github.com>
2021-12-02 20:07:23 +0000

    Add files via upload

---
+++ 01/Part1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AdventOfCode21.Day01
+{
+    class Part1
+    {
+        public static void Run()
+        {
+            int previousMeasurement = 99999;
+            int countIncreased = 0; 
+
+            string[] measurements = System.IO.File.ReadAllLines("01/input");
+            foreach(string measurement in measurements)
+            {
+                int currentMeasurement = int.Parse(measurement);
+                if(currentMeasurement > previousMeasurement)
+                {
+                    countIncreased++;
+                }
+                previousMeasurement = currentMeasurement;
+            }
+            Console.WriteLine(countIncreased);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day01.Part1.Run();
+        }
+    }
+}
